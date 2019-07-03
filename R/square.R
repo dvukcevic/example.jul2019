67a14e76d3b12209b54a074f8c6e3d23aafecb6c
Damjan Vukcevic <damjan.vukcevic@unimelb.edu.au>
2019-07-03 15:57:52 +1000

    Initial commit

---
+++ R/square.R
@@ -0,0 +1,26 @@
+#' Square
+#'
+#' Squares the input
+#'
+#' @param x Input numerical vector
+#'
+#' @return Numerical vector
+#' @export
+#'
+#' @examples
+#' square(2:5)
+square <- function(x) {
+  x^2
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+is.odd <- function(x) {
+  gtools::odd(x)
+}
